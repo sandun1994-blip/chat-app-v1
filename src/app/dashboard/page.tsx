ffaee903e5fb11b53e7ece5e42bff8dc4c3c8077
Sandun Tharuka <sandun.suntws@gmail.com>
2023-05-14 21:38:46 +0530

    setup button component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import Button from "@/components/ui/Button"
+
+
+type Props = {}
+
+const Dashoard = (props: Props) => {
+  return (
+    <div>
+   <Button>
+    fff
+   </Button>
+   
+    </div>
+  )
+}
+
+export default Dashoard
